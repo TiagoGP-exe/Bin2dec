bddc87a036a5169fa532f472c8e9f29a446e8155
Tiago G Pinto <x2bonzao@gmail.com>
2021-05-04 21:15:01 -0400

    finish

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 const txtBinary = document.getElementById('bin')
 const output = document.getElementById('res')
+
 function convert() {
     let valor = (txtBinary).value
-    if (valor != ""){ 
-        let bin = valor.split("") 
-        let verificador = "true"
-
-        if (txtBinary != 0){
-            for (let i = 0; i < bin.length;i++){
-                console.log(bin[i])
-                if(bin[i] != 0 && bin[i] != 1){
-                    verificador = "false"
-                }
-            }
-            if (verificador == "true"){
-                valor = parseInt(valor, 2)
-                output.innerHTML = `${valor}`
+    if (valor != "") {
+        valor.split('').map((char) => {
+            if (char !== '0' && char !== '1') {
+                Toast.show('Por favor digite números binários, Ex:10101', 'error')
             } else {
-                Toast.show('Por favor digite números binários, Ex:10101','error')
+                output.innerHTML = `${parseInt(valor, 2)}`
             }
-        } else {
-        Toast.show('Porfavor insira um valor válido','error')
-        }
+        })
         txtBinary.innerHTML = ``
     } else {
         output.innerHTML = `0`
     }
-    
 }
 
 txtBinary.addEventListener('keyup', e => {
